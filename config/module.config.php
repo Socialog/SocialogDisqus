56b84c2d10699beddc2e609f1eff2cce02e37902
Roy van Kaathoven <royvankaathoven@hotmail.com>
2012-11-04 13:40:10 +0100

    PSR2

--- config/module.config.php
+++ config/module.config.php
@@ -1,15 +1,15 @@
 <?php
 
 return array(
-    
+
     'view_manager' => array(
         'template_path_stack' => array(
             'SocialogDisqus' => __DIR__ . '/../view/',
         ),
     ),
-    
+
     'socialog-disqus' => array(
-        
+
     ),
 
 );
