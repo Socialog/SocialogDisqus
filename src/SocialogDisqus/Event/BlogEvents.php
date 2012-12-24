a150d28d53124aec356f51722a0aeb36c47f7e1d
Roy van Kaathoven <royvankaathoven@hotmail.com>
2012-12-24 22:09:14 +0100

    Added disabling of comments

--- src/SocialogDisqus/Event/BlogEvents.php
+++ src/SocialogDisqus/Event/BlogEvents.php
@@ -3,6 +3,7 @@
 namespace SocialogDisqus\Event;
 
 use Socialog\EventManager\AbstractListenerAggregate;
+use Socialog\Model\ArticleInterface;
 use Zend\EventManager\Event;
 
 /**
@@ -13,35 +14,54 @@ class BlogEvents extends AbstractListenerAggregate
     protected $globalHooks = array(
         'view' => array(
             'post.post@100'         => 'onRenderPost',
+            'page.post@100'         => 'onRenderPost',
             'body.bottom@100'       => 'onRenderBodyBottom',
             'home-post.post@100'    => 'onHomeRenderPost',
         ),
     );
 
     /**
-     * @param \Zend\EventManager\Event $e
-     * @return String
+     * @param Event $e
+     * @return string
      */
     public function onRenderPost(Event $e)
     {
+        $article = $e->getParam('article');
+        
+        if ($article instanceof ArticleInterface && false == $article->getAllowComments()) {
+            return;
+        }
+        
         return $e->getTarget()->partial('socialog-disqus/post', $e->getParams());
     }
 
     /**
-     * @param \Zend\EventManager\Event $e
+     * @param Event $e
      * @return String
      */
     public function onRenderBodyBottom(Event $e)
     {
+        $article = $e->getParam('article');
+        
+        if ($article instanceof ArticleInterface && false == $article->getAllowComments()) {
+            return;
+        }
+
         return $e->getTarget()->partial('socialog-disqus/home/bottom', $e->getParams());
     }
 
     /**
-     * @param \Zend\EventManager\Event $e
+     * @param Event $e
      * @return String
      */
     public function onHomeRenderPost(Event $e)
     {
+        $article = $e->getParam('article');
+        
+        if ($article instanceof ArticleInterface && false == $article->getAllowComments()) {
+            return;
+        }
+        
         return $e->getTarget()->partial('socialog-disqus/home/post', $e->getParams());
     }
 }
